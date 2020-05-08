3f110e89145090162912f3b7e947c154934a351f
Juan Acevedo <juancevedo@gmail.com>
2020-05-07 17:03:29 -0700

    enhance the alphavantage env

--- fingym/examples/environments/using_alphavantage_env.py
+++ fingym/examples/environments/using_alphavantage_env.py
@@ -1,3 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 from fingym import make
+
 if __name__ == '__main__':
   env = make('Alphavantage-Daily-v0',stock_symbol='SPY', alphavantage_key = '')
+  
+  close = np.zeros(env.n_step)
+  obs = env.reset()
+  close[0] = obs[3]
+
+  while True:
+    obs, reward, done, info = env.step([1,1000])
+    close[env.cur_step] = obs[4]
+    if done:
+      print('total reward: {}'.format(info['cur_val']))
+      break
+  
+  time = np.linspace(1, len(close), len(close))
+  plt.plot(time, close, label = 'SPY',linewidth=1.0)
+  plt.title('Alphavantage')
+  plt.legend(loc = 'upper left')
+  plt.show()
